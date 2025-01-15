3c144b551adf187294fef3f7a6bb5b8723ecd3a0
Nour <eldessouky.nour@gmail.com>
2025-01-14 19:45:30 -0500

    First Version of Hello World
    A car object and a form

---
+++ HelloWorldC#/HelloWorldC#/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldC_
+{
+    public class Car
+    {
+        public string make;
+        string model;
+        public int year;
+        public string VIN;
+        public string exteriorColor;
+        public string interiorColor;
+        public Car(string make, string model, int year, string VIN, string exteriorColor, string interiorColor)
+        {
+            this.make = make;
+            this.model = model;
+            this.year = year;
+            this.VIN = VIN;
+            this.exteriorColor = exteriorColor;
+            this.interiorColor = interiorColor;
+        }
+    }
+}
